3e3a8a7e8b45db5f064ebea34af8f0e5ac6edd59
Arayo <arayoprecious@gmail.com>
2025-08-19 22:39:44 +0100

    Qn 14

---
+++ Easy/longest_Common_Prefix.py
@@ -0,0 +1,17 @@
+from typing import List #imported for error purposes
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = ""
+        
+        for x in range (len(strs[0])): #itersting thru every index
+            for y in strs:
+                if x == len(y) or y[x] != strs[0][x]: #adding the first part bc i wanna check if it is within bounds
+                    return prefix
+            prefix = prefix +strs[0][x]
+        
+        return prefix
+
+
+
+        
